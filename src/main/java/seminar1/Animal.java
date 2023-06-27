1990d20b5f0adbf60ef9facbcf0698d2d573f1d5
diniles <dzliashko@gmail.com>
2023-06-27 15:43:34 +0300

    Feat: seminar1 class Animal getters & setters

--- src/main/java/seminar1/Animal.java
+++ src/main/java/seminar1/Animal.java
@@ -27,4 +27,36 @@ public class Animal {
         this.weight = weight;
         this.height = height;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
