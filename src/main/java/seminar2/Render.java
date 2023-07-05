e5448be24f829fc491948978b4a69911a7eb4b0e
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-05 21:16:51 +0300

    Refactor: seminar 2 class Refactor

--- src/main/java/seminar2/Render.java
+++ src/main/java/seminar2/Render.java
@@ -9,6 +9,10 @@ public class Render {
 
     private int total;
     private int current;
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
     public void showIndicator(Object object) {
         // Не должно быть упоминания конкретных классов!!!
@@ -32,6 +36,8 @@ public class Render {
 
     @Override
     public String toString() {
-        return "Total = " + total + ", current = " + current;
+        int percent = (int) Math.ceil((double) this.current / this.total * 100);
+        String color = percent > 60 ? ANSI_GREEN : percent > 20 ? ANSI_YELLOW : ANSI_RED;
+        return String.format("Total = , %d,%s current = %d %s", total, color, current, ANSI_RESET);
     }
 }
