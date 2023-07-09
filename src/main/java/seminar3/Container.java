e5cc7236fa22c1c5e08a492541fefe1b85061d11
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-09 19:14:42 +0300

    Feat: seminar 3 class Container

---
+++ src/main/java/seminar3/Container.java
@@ -0,0 +1,48 @@
+package seminar3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Container implements Comparable<Container>, Iterable<Box> {
+    private List<Box> boxes;
+
+    public Container() {
+        boxes = new ArrayList<Box>();
+    }
+
+    public void addBox(Box box) {
+        boxes.add(box);
+    }
+
+    public int getWeight() {
+        int totalWeight = 0;
+        for (Box box : boxes) {
+            totalWeight += box.getWeight();
+        }
+        return totalWeight;
+    }
+
+    public int getBoxesQuantity() {
+        return this.boxes.size();
+    }
+
+    @Override
+    public int compareTo(Container other) {
+        int thisWeight = this.getWeight();
+        int otherWeight = other.getWeight();
+        if (thisWeight < otherWeight) {
+            return -1;
+        } else if (thisWeight > otherWeight) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public Iterator<Box> iterator() {
+        return boxes.iterator();
+    }
+}
+
