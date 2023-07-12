fe677cd7fc5bd31944f79d18fce2a4b9934f90b3
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-12 17:07:34 +0300

    Feat: seminar 5 class Student

---
+++ src/main/java/seminar5/Student.java
@@ -0,0 +1,43 @@
+package seminar5;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class Student {
+    private final UUID id;
+    private String name;
+
+    public Student(String name) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
