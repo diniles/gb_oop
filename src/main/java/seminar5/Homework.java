49e7be7fcfbed96b7df43ad184ec1a05d6bedced
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-12 18:07:36 +0300

    Feat: seminar 5 я хз, совсем запутался!

--- src/main/java/seminar5/Homework.java
+++ src/main/java/seminar5/Homework.java
@@ -1,4 +1,8 @@
 package seminar5;
 
 public class Homework {
+    public static void main(String[] args) {
+        Student student = new Student("DL");
+        System.out.println(student);
+    }
 }
