dba6995a20c7efdf469f3cdc4cbe7b49b518367c
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-12 21:17:35 +0300

    Feat: seminar 5 start from zero

--- src/main/java/seminar5/Homework.java
+++ src/main/java/seminar5/Homework.java
@@ -2,7 +2,6 @@ package seminar5;
 
 public class Homework {
     public static void main(String[] args) {
-        Student student = new Student("DL");
-        System.out.println(student);
+
     }
 }
--- src/main/java/seminar5/Student.java
+++ src/main/java/seminar5/Student.java
@@ -1,35 +1,7 @@
 package seminar5;
 
-import java.util.Objects;
-import java.util.UUID;
 
 public class Student {
-    private final UUID id;
-    private final String name;
 
-    public Student(String name) {
-        this.id = UUID.randomUUID();
-        this.name = name;
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Student student = (Student) o;
-        return Objects.equals(id, student.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
