b65b4cb51590c94b8b67edc2ad2358f5cd829681
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-12 17:10:36 +0300

    Refactor: seminar 5 class Student

--- src/main/java/seminar5/Student.java
+++ src/main/java/seminar5/Student.java
@@ -12,14 +12,6 @@ public class Student {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
