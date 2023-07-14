c965f6c9e78313533abab9a574e344a279dd90a6
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-14 17:42:37 +0300

    Feat: seminar 5 class Student

--- src/main/java/seminar5/Student.java
+++ src/main/java/seminar5/Student.java
@@ -1,5 +1,39 @@
 package seminar5;
 
 public class Student {
+    private static long count = 1;
+    private long id;
+    private String name;
+    private long groupId;
 
+    public Student(String name) {
+        this.id = count + 1;
+        this.name = name;
+        this.groupId = 0;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", groupId=" + groupId +
+                '}';
+    }
 }
