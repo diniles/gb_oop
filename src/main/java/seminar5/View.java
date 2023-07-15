c9d92ffadffda825e3a069610ab00932e762847d
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-15 17:50:53 +0300

    Feat: seminar 5 View

---
+++ src/main/java/seminar5/View.java
@@ -0,0 +1,18 @@
+package seminar5;
+
+public class View {
+
+    public void menu() {
+        System.out.println("Choose action:");
+        System.out.println("\t1. Create group");
+        System.out.println("\t2. List all groups");
+        System.out.println("\t3. Create student in a group");
+        System.out.println("\t4. List all students in the group");
+        System.out.println("\t5. Create a lesson for the group");
+        System.out.println("\t6. List all lessons for the student");
+        System.out.println("\t0. Exit");
+
+    }
+}
+
+
