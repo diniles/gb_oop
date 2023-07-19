302d692fb0a8b0df889a608999dc018721268a05
diniles <dzliashko@gmail.com>
2023-07-19 15:29:29 +0300

    Feat: seminar6 class Report

--- src/main/java/seminar6/Report.java
+++ src/main/java/seminar6/Report.java
@@ -2,4 +2,16 @@ package seminar6;
 
 public class Report {
     private byte[] data;
+
+    Document document;
+    String format;
+
+    public Report(Document document, String format) {
+        this.document = document;
+        this.format = format;
+    }
+
+    public byte[] getReportData() {
+        return this.data;
+    }
 }
