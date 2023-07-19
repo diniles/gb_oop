d4030280e9486c034537599c192995b03830b746
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-19 19:07:31 +0300

    Feat: seminar 6 class Homework

--- src/main/java/seminar6/Homework.java
+++ src/main/java/seminar6/Homework.java
@@ -4,6 +4,8 @@ public class Homework {
     public static void main(String[] args) {
         Document document = new Document("100500");
 
-        
+        ReportEntrypoint reportEntrypoint = new ReportEntrypoint();
+        Report report = reportEntrypoint.create(document, "pdf");
+
     }
 }
