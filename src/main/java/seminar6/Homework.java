bcc576eb40500489c7d37feff7ba7eae6e337fbd
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-19 19:26:19 +0300

    Feat: seminar 6 finished

--- src/main/java/seminar6/Homework.java
+++ src/main/java/seminar6/Homework.java
@@ -5,9 +5,14 @@ public class Homework {
         Document document = new Document("100500");
 
         ReportEntrypoint reportEntrypoint = new ReportEntrypoint();
-        Report report = reportEntrypoint.create(document, "pdf");
-        
-        report.printData();
+        Report reportPdf = reportEntrypoint.create(document, "pdf");
+        Report reportJson = reportEntrypoint.create(document, "json");
+        Report reportXml = reportEntrypoint.create(document, "xml");
+//        Report reportError = reportEntrypoint.create(document, "Error");
 
+        reportPdf.printData();
+        reportJson.printData();
+        reportXml.printData();
+//        reportError.printData();
     }
 }
