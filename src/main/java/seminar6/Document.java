17f359b0b4b92363cd111450fc95db3e4c504d96
Dzmitry Liashko <dzliashko@gmail.com>
2023-07-19 19:26:04 +0300

    Refactor: seminar 6

--- src/main/java/seminar6/Document.java
+++ src/main/java/seminar6/Document.java
@@ -3,8 +3,8 @@ package seminar6;
 import java.util.UUID;
 
 public class Document {
-    private UUID id;
-    private String number;
+    private final UUID id;
+    private final String number;
 
     public Document(String number) {
         this.id = UUID.randomUUID();
