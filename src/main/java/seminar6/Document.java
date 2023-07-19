41577c1a15bba6f20d855618bade03aea564c3bf
diniles <dzliashko@gmail.com>
2023-07-19 15:03:25 +0300

    Feat: seminar6 class Document

--- src/main/java/seminar6/Document.java
+++ src/main/java/seminar6/Document.java
@@ -5,4 +5,25 @@ import java.util.UUID;
 public class Document {
     private UUID id;
     private String number;
+
+    public Document(String number) {
+        this.id = UUID.randomUUID();
+        this.number = number;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
 }
